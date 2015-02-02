e8702f23d42b2e384709c61f2e38e5d57ae25ea3
Adrian Rossouw <adrian@daemon.co.za>
2015-02-02 21:57:04 +0000

    system compile

--- definitions/services.js
+++ definitions/services.js
@@ -25,7 +25,7 @@ exports.logspout = {
   }
 };
 
-var configFile = 'https://gist.githubusercontent.com/AdrianRossouw/f1d56196ed1ce56f4d3f/raw/644f40731a416870ac801e5e9fd2418129e7745d/logstash.config';
+var configFile = 'https://gist.githubusercontent.com/AdrianRossouw/f1d56196ed1ce56f4d3f/raw/8b60db9e395c03f4dd853cca8ee74ff979f2eb04/logstash.config';
 
 exports.elk = {
   type: 'docker',
