852aa9b8d32bcb0d63848d33f7ce7cfd08f82b0c
Adrian Rossouw <adrian@daemon.co.za>
2015-02-02 20:37:25 +0000

    system compile

--- definitions/services.js
+++ definitions/services.js
@@ -30,7 +30,7 @@ exports.elk = {
   specific: {
     name: 'pblittle/docker-logstash',
     execute: {
-      args: '-d -p 5515:5515/udp -p 9292:9292 -p 9200:9200 -e LOGSTASH_CONFIG_URL=https://gist.githubusercontent.com/AdrianRossouw/f1d56196ed1ce56f4d3f/raw/8915847f1830fa59b64ed701214152bf17ea0c51/logstash.config'
+      args: '-d -p 5515:5515/udp -p 9292:9292 -p 9200:9200 -e LOGSTASH_CONFIG_URL=https://gist.githubusercontent.com/AdrianRossouw/f1d56196ed1ce56f4d3f/raw/213e86c8595c3584a9a07814130fc04d32e2aa12/logstash.config'
     }
   }
 };
